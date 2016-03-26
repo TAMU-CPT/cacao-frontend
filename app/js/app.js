c2d856664f6c2b735c91c67d5ad55bf4be782a7f
Eleni Mijalis <elenimijalis@gmail.com>
2016-03-25 21:36:11 -0500

    restangular functionality

--- app/js/app.js
+++ app/js/app.js
@@ -2,27 +2,61 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatAnimations',
-
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
+var cacaoApp = angular.module('cacaoApp', [
+    'ngRoute',
+    'restangular'
 ]);
 
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+cacaoApp.config(['$routeProvider', 'RestangularProvider',
+    function($routeProvider, RestangularProvider) {
+        $routeProvider.
+            when('/teams', {
+                templateUrl: 'partials/team-list.html',
+                controller: 'TeamListCtrl'
+            }).
+            when('/teams/:teamID', {
+                templateUrl: 'partials/team-detail.html',
+                controller: 'TeamDetailCtrl'
+            }).
+            otherwise({
+                redirectTo: '/teams'
+            });
+        RestangularProvider.setBaseUrl('http://localhost:8000/');
+        RestangularProvider.setRequestSuffix('/');
+        RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+            var extractedData;
+            // .. to look for getList operations
+            if (operation === "getList") {
+            // .. and handle the data and meta data
+                extractedData = data.results;
+                extractedData.meta = {
+                    'count': data.count,
+                    'next': data.next,
+                    'previous': data.previous
+                }
+            } else {
+                extractedData = data;
+            }
+            return extractedData;
+        });
+}]);
+
+cacaoApp.controller('TeamListCtrl', ['$scope', 'Restangular',
+    function($scope, Restangular) {
+        var promise = Restangular.all('groups').getList();
+        var resolveGroup = function(data) {
+            $scope.teams = data;
+        }
+        promise.then(resolveGroup);
+}]);
+
+cacaoApp.controller('TeamDetailCtrl', ['$scope', '$routeParams', 'Restangular',
+    function($scope, $routeParams, Restangular) {
+        var promise = Restangular.one('groups', $routeParams.teamID).get();
+
+        var resolveGroup = function(data) {
+            $scope.team = data;
+        }
+
+        promise.then(resolveGroup);
+}]);
