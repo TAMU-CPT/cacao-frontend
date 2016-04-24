62124fb9f59105c56a54675f307bfb39b066cddc
Eric Rasche <rasche.eric@gmail.com>
2016-04-24 21:26:49 +0000

    Decode user data

--- app/js/app.js
+++ app/js/app.js
@@ -135,6 +135,8 @@ cacaoApp.controller('LoginCtrl', ['$scope', '$http', '$localStorage', '$location
                 $http.post('http://localhost:8000/api-token-auth/', userData)
                     .success(function(data) {
                         $localStorage.jwtToken = data.token;
+                        // $localStorage.jwtData = {username: "admin", user_id: 1, email: "user@host", exp: 1462137950}
+                        $localStorage.jwtData = jwt_decode(data.token);
                         $location.path('/');
                     })
                     .error(function() {
