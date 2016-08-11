5a345950417bc18c9409ae773382939d636bcbbb
Eric Rasche <rasche.eric@gmail.com>
2016-08-11 23:46:03 +0000

    Going nuts

--- js/app.js
+++ js/app.js
@@ -131,7 +131,8 @@ cacaoApp.config(['$routeProvider', '$httpProvider', '$mdThemingProvider', 'grava
                 controller: 'LogOutCtrl'
             }).
             when('/', {
-                templateUrl: 'partials/home.html'
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
             }).
             otherwise({
                 redirectTo: '/'
@@ -943,6 +944,58 @@ cacaoApp.controller('ReviewCtrl', ['$scope', 'CacaoBackend', '$timeout', '$filte
                 flagged: flagged.join(),
             });
         };
+}]);
+
+cacaoApp.controller('HomeCtrl', ['$scope',
+    function($scope) {
+        $scope.home = [
+            {
+                title: "CACAO",
+                url: "#/about",
+                description: "Welcome to CACAO! Here you will annotate proteins with Gene Ontology Terms. Yay!",
+                button_title: "Get excited!",
+            },
+            {
+                title: "Users",
+                url: "#/users",
+                description: "Browse users registered in the CACAO System",
+            },
+            {
+                title: "Teams",
+                url: "#/teams",
+                description: "Check out the competition!",
+            },
+            {
+                title: "Review",
+                url: "#/review",
+                description: "Review student annotations.",
+                button_title: "I am an admin",
+            },
+            {
+                title: "GAF Create",
+                url: "#/gaf/create",
+                description: "Submit a new Annotation",
+            },
+            {
+                title: "GAF List",
+                url: "#/gaf/list",
+                description: "See other annotations.",
+            },
+            {
+                title: "PMID #1",
+                url: "#/pmid/1",
+            },
+            {
+                title: "GO:0000001",
+                url: "#/goid/GO:0000001"
+            },
+            {
+                title: "Testing",
+                url: "#/test",
+                description: "Here be dragons",
+                button_title: "I have been warned",
+            },
+        ];
 }]);
 
 cacaoApp.controller('GAFListCtrl', ['$scope', 'CacaoBackend',
