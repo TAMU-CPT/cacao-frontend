8103eeece7740f5774bd54c887f4bcc7a13ee58b
Eleni Mijalis <elenimijalis@gmail.com>
2016-08-25 12:50:59 -0500

    ordering not working

--- js/ctrl/user/list.js
+++ js/ctrl/user/list.js
@@ -1,8 +1,28 @@
 export default function(cacaoApp) {
     cacaoApp.controller('UserListCtrl', ['$scope', 'CacaoBackend',
         function($scope, CacaoBackend) {
-            CacaoBackend.all('users').getList().then(function(data) {
-                $scope.users = data;
-            });
+            $scope.ordering="username";
+            $scope.updateData = function(page) {
+                if(!isNaN(parseInt(page))){
+                    $scope.query.page = page;
+                }
+                $scope.query.ordering = $scope.ordering;
+                $scope.promise = CacaoBackend.all('users').getList($scope.query).then(function(data) {
+                    $scope.users = data;
+                });
+            };
+
+            $scope.options = {
+                limitSelect: true,
+                pageSelect: true
+            };
+
+            $scope.query = {
+                limit: 5,
+                page: 1,
+                ordering: $scope.ordering,
+            };
+
+            $scope.updateData(1);
     }]);
 }
