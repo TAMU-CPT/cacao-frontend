d3046b44959c0814710f398b39c28ec7f1018e11
Eleni Mijalis <elenimijalis@gmail.com>
2016-08-30 12:08:04 -0500

    gene links to gaf create

--- js/ctrl/search/gene.js
+++ js/ctrl/search/gene.js
@@ -1,6 +1,13 @@
 export default function(cacaoApp) {
-    cacaoApp.controller('SearchGeneCtrl', ['$scope',
-        function($scope) {
-            console.log('gene');
+    cacaoApp.controller('SearchGeneCtrl', ['$scope', 'CacaoBackend', '$location',
+        function($scope, CacaoBackend, $location) {
+            CacaoBackend.all('genes').getList($scope.query).then(function(data) {
+                $scope.genes = data;
+            });
+
+            $scope.create_gaf = function(gene) {
+                console.log(gene);
+                $location.path('/gaf/create/').search({taxon: gene.organism.taxon, gene: gene.db_object_id});
+            };
     }]);
 }
--- js/ctrl/search/organism.js
+++ js/ctrl/search/organism.js
@@ -1,6 +1,8 @@
 export default function(cacaoApp) {
-    cacaoApp.controller('SearchOrganismCtrl', ['$scope',
-        function($scope) {
-            console.log('organism');
+    cacaoApp.controller('SearchOrganismCtrl', ['$scope', 'CacaoBackend',
+        function($scope, CacaoBackend) {
+            CacaoBackend.all('organisms').getList($scope.query).then(function(data) {
+                $scope.organisms = data;
+            });
     }]);
 }
