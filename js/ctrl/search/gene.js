142477e6e89de1f3260656934ef42f6f538321a1
Eleni Mijalis <elenimijalis@gmail.com>
2016-09-02 08:33:58 -0500

    gene table/ordering

--- js/ctrl/search/gene.js
+++ js/ctrl/search/gene.js
@@ -1,13 +1,36 @@
 export default function(cacaoApp) {
     cacaoApp.controller('SearchGeneCtrl', ['$scope', 'CacaoBackend', '$location', '$routeParams',
         function($scope, CacaoBackend, $location, $routeParams) {
-            CacaoBackend.all('genes').getList({org_id: $routeParams.orgID}).then(function(data) {
-                $scope.genes = data;
-            });
 
             $scope.create_gaf = function(gene) {
-                //$location.path('/gaf/create/').search({taxon: gene.organism.taxon, gene: gene.db_object_id});
                 $location.path('/gaf/create/').search({gene_id: gene.id});
             };
+
+            $scope.ordering = "start";
+
+            $scope.updateData = function(page) {
+                if(!isNaN(parseInt(page))){
+                    $scope.query.page = page;
+                }
+                $scope.query.ordering = $scope.ordering;
+                console.log(page);
+
+                $scope.promise = CacaoBackend.all('genes').getList($scope.query).then(function(data) {
+                    $scope.genes = data;
+                });
+            };
+            $scope.options = {
+                limitSelect: true,
+                pageSelect: true
+            };
+
+            $scope.query = {
+                org_id: $routeParams.orgID,
+                limit: 5,
+                page: 1,
+                ordering: $scope.ordering,
+            };
+
+            $scope.updateData(1);
     }]);
 }
