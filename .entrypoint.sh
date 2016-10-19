2ad73214f8af5f1a96fad39cb8ebdb0bf59c3a53
Eric Rasche <rasche.eric@gmail.com>
2016-10-19 20:42:51 +0000

    For rancher

--- .entrypoint.sh
+++ .entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-sed -i "s|http://localhost:8000|$CACAO_BACKEND_URL|g" /var/www/html/build/app.js
-nginx -g "daemon off;"
+sed -i "s|http://localhost:8000|$CACAO_BACKEND_URL|g" /output/build/app.js
+echo "This container is complete, please serve the files from a static file server container. They have been build in /output/"
