89d13b3053bbf71adbddcaed85d1631aa73259af
Eric Rasche <rasche.eric@gmail.com>
2017-02-28 00:20:27 +0000

    Auto-login if remote_user

--- js/ctrl/nav.js
+++ js/ctrl/nav.js
@@ -1,6 +1,6 @@
 export default function(cacaoApp) {
-    cacaoApp.controller('NavCtrl', ['$scope', '$mdSidenav', '$localStorage', '$location', 'CacaoBackend', 'NotificationBackend', '$interval',
-        function ($scope, $mdSidenav, $localStorage, $location, CacaoBackend, NotificationBackend, $interval) {
+    cacaoApp.controller('NavCtrl', ['$scope', '$mdSidenav', '$localStorage', '$location', 'CacaoBackend', 'NotificationBackend', '$interval', '$http', 'DRF_URL', 'REMOTE_USER',
+        function ($scope, $mdSidenav, $localStorage, $location, CacaoBackend, NotificationBackend, $interval, $http, DRF_URL, REMOTE_USER) {
             $scope.nav = {}
             $scope.nav.userData = $localStorage.jwtData;
 
@@ -21,6 +21,24 @@ export default function(cacaoApp) {
                 }
                 else { $location.path(route); }
             };
+
+            // If remote user mode is turned on and there is no JWT token
+            if(REMOTE_USER && $localStorage.jwtToken !== undefined){
+                // Then we make a quick request to check who we are
+                $http.get(DRF_URL + 'api-token-auth-whoami/')
+                    .success(function(data){
+                        // Which, if it succeeds, means we are auth'd under REMOTE_USER
+                        $localStorage.jwtToken = 'REMOTE_USER';
+                        // And we can just set our data.
+                        $localStorage.jwtData = {
+                            username: data.username,
+                            user_id: data.id,
+                            email: data.username,
+                            exp: null
+                         };
+                        $scope.nav.userData = $localStorage.jwtData;
+                    });
+            }
 
             $scope.get_notifications = function() {
                 NotificationBackend.all('inbox').getList().then(function(data) {
